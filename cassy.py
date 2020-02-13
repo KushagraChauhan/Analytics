bbf0eafc4553afabd7f27538701f83a718857c98
KushagraChauhan <kushagra_sc@yahoo.in>
2020-02-13 18:08:26 +0530

    dev cassandra flask

--- cassy.py
+++
@@ -1,9 +0,0 @@
-'''this is cassandra db connection with python'''
-
-from cassandra.cluster import Cluster
-
-cluster = Cluster() # cluster = Cluster(['192.168.0.1', '192.168.0.2']) to specidy IP
-
-#establish a connection
-session = cluster.connect()
-rows = session.execute('')
