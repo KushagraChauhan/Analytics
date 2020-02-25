b1cf9dd485f6602f0b4519f28acc58cd8b63cb5b
KushagraChauhan <kushagra_sc@yahoo.in>
2020-02-25 15:11:25 +0530

    on server codes

---
+++ onserver/app.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, jsonify, request
+import json
+import geoip2.database as geo
+import os
+import uuid
+from flask_cqlalchemy import CQLAlchemy
+
+
+app = Flask(__name__)
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "test"
+db = CQLAlchemy(app)
+
+################################################################################
+'''Get details of the user through JS'''
+'''Store the details in an image of 1px and send to the Flask app'''
+################################################################################
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/details", methods=['POST'])
+def rec_img():
+    print("received img")
+    rf = request.form
+    print(rf)
+    for key in rf.keys():
+        data=key
+    print("The data is----------",data)
+    data_dic = json.loads(data)
+    print("............",data_dic.keys())
+    img_url = data_dic['url']
+    img_width = data_dic['width']
+    img_height = data_dic['height']
+    img_plaform = data_dic['platform']
+    img_history = data_dic['history']
+    img_ref = data_dic['ref']
+    img_nav = data_dic['nav']
+    img_plugins = data_dic['_plugins']
+    print('<<<<<<',img_url,'>>>>>>>')
+    print('<<<<<<',img_width,'>>>>>>>')
+    print('<<<<<<',img_height,'>>>>>>>')
+    print('<<<<<<',img_plaform,'>>>>>>>')
+    print('<<<<<<',img_history,'>>>>>>>')
+    print('<<<<<<',img_ref,'>>>>>>>')
+    print('<<<<<<',img_nav,'>>>>>>>')
+    ip = request.remote_addr
+    resp_dic={'url':img_url, 'msg':"details:",
+    'width':img_width, 'height':img_height}
+    resp = jsonify(resp_dic)
+
+    resp.headers['Access-Control-Allow-Origin']='*'
+    #print('<<<<<<<<<',resp_dic,'>>>>>>>>>>>')
+    ##############
+    '''Storing the data in the Cassandra db'''
+    ##############
+
+    Users.objects(id).create(url=img_url, ref=img_ref, width=img_width,
+    height=img_height, platform=img_plaform, history=img_history, ip=ip)
+    return resp
+
+################################################################################
+'''Structure of Cassandra database'''
+################################################################################
+class Users(db.Model):
+    __keyspace__ = 'test'
+    #uid = db.columns.UUID(primary_key = True, default = uuid.uuid4)
+    id = db.columns.Integer(primary_key = True)
+    url = db.columns.Text()
+    ref = db.columns.Text()
+    nav = db.columns.Text()
+    width = db.columns.Integer()
+    height = db.columns.Integer()
+    platform = db.columns.Text()
+    history = db.columns.Integer()
+    ip = db.columns.Text()
+################################################################################
+'''get the geolocation of the user'''
+################################################################################
+@app.route("/ip", methods=["GET"])
+def location():
+    ip = request.remote_addr
+    return getCity(ip) +' ' + getCountry(ip)
+
+def getCity(ip):
+    ip = request.remote_addr
+    MYDIR = os.path.dirname(os.path.abspath(__file__))
+    data=os.path.join(MYDIR, "GeoLite2-City.mmdb")
+    reader = geo.Reader(data)
+    response = reader.city(ip)
+    return response.city.name
+def getCountry(ip):
+    ip = request.remote_addr
+    MYDIR = os.path.dirname(os.path.abspath(__file__))
+    data1=os.path.join(MYDIR, "GeoLite2-Country.mmdb")
+    reader = geo.Reader(data1)
+    response = reader.country(ip)
+    return response.country.name
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
