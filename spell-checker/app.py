9ecca69b7fd74df5ee0935fa83012c2a3fce089f
KushagraChauhan <kushagra_sc@yahoo.in>
2020-05-18 12:06:42 +0530

    #925 correction on search bar implemented

--- spell-checker/app.py
+++ spell-checker/app.py
@@ -3,19 +3,24 @@ Issue: #925
 This is a basic spell checker which is to be used in the Experience Centre
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from textblob import TextBlob
 from textblob import Word
 
 
 app = Flask(__name__)
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 
 @app.route('/correction')
 def correction():
-    text = request.args.get('text', '')
+    text = request.args.get('search', '')
     text = TextBlob(text)
-    return jsonify(text=unicode(text.correct()))
+    corr_text = text.correct()
+    return jsonify(str(corr_text))
 
 if __name__ == '__main__':
     # app runs in debug mode, turn this off if you're deploying
